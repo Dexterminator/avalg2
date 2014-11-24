4e0d71c518f3b701946ea1bc2391c9797ae61eb1
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-11-24 14:30:56 +0100

    Initial swag.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+/**
+ * Created by dexter on 24/11/14.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("swag");
+    }
+}
