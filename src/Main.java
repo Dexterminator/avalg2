ac1ec11459677e527c3516c38868be9c591ab4bd
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-11-25 08:42:21 +0100

    Added sample file and input reading.

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,16 @@ import java.io.IOException;
  * Created by dexter on 24/11/14.
  */
 public class Main {
+    static Kattio io;
+
 
     public static void main(String[] args) throws IOException {
-        System.out.println("swag");
+        io = new Kattio(System.in, System.out);
+        int pointsCount = io.getInt();
+        for (int i = 0; i < pointsCount; i++) {
+            double coord1 = io.getDouble();
+            double coord2 = io.getDouble();
+            System.out.println(coord1 + " " + coord2);
+        }
     }
 }
